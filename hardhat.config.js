752e46c919f62781b2bf12dfb18c16cc14a80c89
arya-metageeks <shubham.arya@metageeks.tech>
2024-11-20 13:23:27 +0530

    update: hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); // For environment variables
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,10 +9,22 @@ module.exports = {
       viaIR: true,
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
-  }
-  };
-
-
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "localhost",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545", // Local Hardhat network
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "", // RPC URL for Sepolia
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [], // Your private key
+      chainId: 11155111, // Chain ID for Sepolia
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "", // For verifying contracts
+  },
+};
